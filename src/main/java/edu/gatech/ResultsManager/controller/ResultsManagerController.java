dae81012c1c3c8880a84ccdcffb452521f677d41
Riley <Michael.Riley@gtri.gatech.edu>
2019-03-05 14:36:51 -0500

    Added identifier service, and changed the main PACERflow to handle identifiers instead of ids.

--- src/main/java/edu/gatech/ResultsManager/controller/ResultsManagerController.java
+++ src/main/java/edu/gatech/ResultsManager/controller/ResultsManagerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -16,34 +17,44 @@ import edu.gatech.ResultsManager.FHIR2ECR.service.CQLFHIR2ECRService;
 import edu.gatech.ResultsManager.cql.execution.service.CQLExecutionService;
 import edu.gatech.ResultsManager.cql.storage.service.CQLStorageService;
 import edu.gatech.ResultsManager.ecr.storage.service.ECRStorageService;
+import edu.gatech.ResultsManager.fhir.identifier.service.PatientIdentifierService;
 import gatech.edu.STIECR.JSON.ECR;
 
 @RestController
 public class ResultsManagerController{
 
 	CQLStorageService cqlStorageService;
+	PatientIdentifierService patientIdentifierService;
 	CQLExecutionService cqlExecutionService;
 	//ECRStorageService ecrStorageService;
 	CQLFHIR2ECRService cqlFhir2EcrService;
 	
 	@Autowired
-	public ResultsManagerController(CQLStorageService cqlStorageService, CQLExecutionService cqlExecutionService,
+	public ResultsManagerController(CQLStorageService cqlStorageService, PatientIdentifierService patientIdentifierService, CQLExecutionService cqlExecutionService,
 			CQLFHIR2ECRService cqlFhir2EcrService) {
 		super();
 		this.cqlStorageService = cqlStorageService;
+		this.patientIdentifierService = patientIdentifierService;
 		this.cqlExecutionService = cqlExecutionService;
 		this.cqlFhir2EcrService = cqlFhir2EcrService;
 	}
 	
 	@RequestMapping(value = "/case", method = RequestMethod.POST)
-	public ResponseEntity<ECR> pacerFlow(@RequestParam(value = "firstName", required = true) String firstName,
-			@RequestParam(value = "lastName", required = true) String lastName,
-			@RequestParam(value = "patientId", required = true) String id,
+	public ResponseEntity<ECR> pacerFlow(
+			@RequestParam(value =  "firstName", required = false) String firstName,
+			@RequestParam(value =  "lastName", required = false) String lastName,
+			@RequestParam(value = "identifier", required = true) String identifier,
 			@RequestParam(value = "cqlType", required = true) String cqlName){
 		String cqlBody = cqlStorageService.requestCQL(cqlName);
 		//ECR ecr = ecrStorageService.getECR(firstName, lastName);
 		ECR ecr = new ECR();
-		JsonNode cqlResults = cqlExecutionService.evaluateCQL(cqlBody,id);
+		String patientId;
+		try {
+			patientId = patientIdentifierService.getFhirIdByIdentifier(identifier);
+		} catch (Exception e) {
+			return new ResponseEntity<ECR>(ecr,HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		JsonNode cqlResults = cqlExecutionService.evaluateCQL(cqlBody,patientId);
 		ECR ecrFromCQL = cqlFhir2EcrService.CQLFHIRResultsToECR((ArrayNode)cqlResults);
 		ecr.update(ecrFromCQL);
 		//ecrStorageService.storeECR(ecr.toString());
@@ -56,6 +67,14 @@ public class ResultsManagerController{
 
 	public void setCqlStorageService(CQLStorageService cqlStorageService) {
 		this.cqlStorageService = cqlStorageService;
+	}
+
+	public PatientIdentifierService getPatientIdentifierService() {
+		return patientIdentifierService;
+	}
+
+	public void setPatientIdentifierService(PatientIdentifierService patientIdentifierService) {
+		this.patientIdentifierService = patientIdentifierService;
 	}
 
 	public CQLExecutionService getCqlExecutionService() {
